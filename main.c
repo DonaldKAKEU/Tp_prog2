3e348ece9eaae899b9dde409868fdcf12ff19016
Kandy <donaldandy43@gmail.com>
2022-12-10 02:05:36 +0100

    Ordonancement des processeurs

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "proces.h"
+
+int main(void) 
+{
+
+	// On lance l'ordonnanceur
+	run();
+	printf("Fin du programme\n");
+	
+
+  return 0;
+
+}
+
+
